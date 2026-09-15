43ae445e71cadfdf938aac8707a6edda9ab4c1ba
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-09-15 22:42:05 +0530

    Create 158A_Next_round.py

---
+++ 158A_Next_round.py
@@ -0,0 +1,14 @@
+#Problem: 158A - Next Round
+#Plan: Count the number of participants who advance to the next round based on their scores.
+#Platform: Codeforces
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+count = 0
+
+for x in a:
+    if x >= a[k - 1] and x > 0:
+        count += 1
+
+print(count)
